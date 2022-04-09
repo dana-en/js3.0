f548e4c70ae304f8e7dcc5de53c6bcd75555913e
dana-en <dayna.en227@gmail.com>
2022-04-09 22:37:31 +0600

    init

---
+++ script.js
@@ -0,0 +1,60 @@
+//1
+let num = 2
+console.log( num )
+
+//2
+let a = 10
+let b = 2
+let c = 5
+console.log( a + b + c )
+
+//3
+let j = 100 * 2
+console.log(100 * 2)
+
+//4
+let f = 67 / 7
+console.log(67 / 7)
+
+//5
+let p = 10
+let r = 2
+let exam1 = p + r
+let exam2 = p - r
+let exam3 = p * r
+let exam4 = p / r
+console.log(exam1, exam2, exam3, exam4)
+
+//6
+let i = 15
+let m = 2
+let result = i + m
+console.log(result)
+
+//7
+let e = 30
+let s = 15
+let n = e + s
+console.log( n / 4)
+
+//8
+let w = 17
+let k = 10
+let v = w - k
+let x = 69
+let dn = v + x
+console.log( dn )
+
+//9
+let t = "10"
+let g = "99"
+let accurate = Number(t) + Number(g)
+console.log(accurate)
+
+//10
+let name ="лет"
+let number = 10
+let addition = name + number
+console.log(addition)
+
+
